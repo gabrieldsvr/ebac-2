85d1cd239b0f9894859560ffcd5305ab1aed6b7b
gabrieldsvr <gabrieldsvrcontato@gmail.com>
2023-08-10 23:44:41 -0300

    Faz a leitura de teclas

--- js/main.js
+++ js/main.js
@@ -94,4 +94,29 @@ function teclaResult() {
         operacaoSinal = 5;
 
     }
+
+
+
+
 }
+window.addEventListener("keydown", function(event) {
+    if (event.code === "Numpad0" || event.code === "Numpad1" || event.code === "Numpad2" || event.code === "Numpad3" || event.code === "Numpad4" || event.code === "Numpad5" || event.code === "Numpad6" || event.code === "Numpad7" || event.code === "Numpad8" || event.code === "Numpad9" || event.code === "Digit0" || event.code === "Digit1" ||event.code === "Digit2"|| event.code === "Digit3" || event.code === "Digit4" || event.code === "Digit5" || event.code === "Digit6" || event.code === "Digit7" ||event.code === "Digit8" ||event.code === "Digit9") {
+        teclaNumeroTela(event.key);
+    }
+
+    if (event.code === "NumpadAdd") {
+        teclaOperacao(1);
+    }
+    if (event.code === "NumpadSubtract") {
+        teclaOperacao(2);
+    }
+    if (event.code === "NumpadMultiply") {
+        teclaOperacao(3);
+    }
+    if (event.code === "NumpadDivide") {
+        teclaOperacao(4);
+    }
+    if (event.code === "NumpadEnter"  || event.code === "Enter") {
+        teclaResult();
+    }
+});
